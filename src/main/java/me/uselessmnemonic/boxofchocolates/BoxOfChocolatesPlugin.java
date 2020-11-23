c80360abd05f95ccb448f575914859e80d0b6700
Christopher Madrigal <c4madrig@ucsd.edu>
2020-11-22 16:17:14 -0800

    addeded Dark Chocolate Carmel Added abstract chocolate class

--- src/main/java/me/uselessmnemonic/boxofchocolates/BoxOfChocolatesPlugin.java
+++ src/main/java/me/uselessmnemonic/boxofchocolates/BoxOfChocolatesPlugin.java
@@ -1,7 +1,16 @@
 package me.uselessmnemonic.boxofchocolates;
 
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import me.uselessmnemonic.boxofchocolates.chocolates.DarkChocolateCarmel;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BoxOfChocolatesPlugin extends JavaPlugin implements SlimefunAddon {
@@ -29,6 +38,25 @@ public class BoxOfChocolatesPlugin extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
+
+        // create our chocolate category
+        NamespacedKey categoryKey = BoxOfChocolatesPlugin.getNamespacedKey("Chocolates");
+        CustomItem categoryItem = new CustomItem(Material.COCOA, ChatColor.WHITE + "Dulces de chocolate!");
+        Category chocolateCategory = new Category(categoryKey, categoryItem);
+
+        // create the in-game item
+        SlimefunItemStack itemStack = new SlimefunItemStack("DarkChocolateCarmel", Material.COCOA, "Dark Chocolate Carmel", "Bitter chocolate with a", "gooey carmel center.");
+
+        // a crafting recipe (3x3)
+        ItemStack[] recipe = {
+                new ItemStack(Material.COCOA_BEANS), new ItemStack(Material.SUGAR), null,
+                null, null, null,
+                null, null, null
+        };
+
+        // introduce item into game
+        DarkChocolateCarmel darkChocolateCarmel = new DarkChocolateCarmel(chocolateCategory, itemStack, RecipeType.NULL, recipe);
+        darkChocolateCarmel.register(this);
     }
 
     @Override
@@ -40,4 +68,5 @@ public class BoxOfChocolatesPlugin extends JavaPlugin implements SlimefunAddon {
     public String getBugTrackerURL() {
         return "https://github.com/UselessMnemonic/BoxOfChocolates/issues";
     }
+
 }
---
+++ src/main/java/me/uselessmnemonic/boxofchocolates/chocolates/DarkChocolateCarmel.java
@@ -0,0 +1,14 @@
+package me.uselessmnemonic.boxofchocolates.chocolates;
+
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.inventory.ItemStack;
+
+public class DarkChocolateCarmel extends AbstractChocolate {
+
+    public DarkChocolateCarmel(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(category, item, recipeType, recipe);
+    }
+
+}
